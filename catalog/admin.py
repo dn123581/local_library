bf88a84f26bb7bb9cb0ac56f86030134052e56d8
diver <dn123581@gmail.com>
2023-01-18 10:53:56 +0700

    add catalog app

---
+++ catalog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Book, BookInstance, Genre
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+
+
